3ad004a94e99b579b103974c9b0e73436b085b3d
Yosr Naija <yosr.naija@live.com>
2022-06-02 21:50:18 +0100

    login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./App.css"
 import Hello from "./components/hello/Hello"
 import TaskPage from "./pages/tasksPage/TaskPage"
@@ -11,21 +11,65 @@ import {
 import Menu from "./components/menu/Menu"
 import TaskDetails from "./components/taskDetails/TaskDetails"
 import TeacherRoutes from "./components/teacherRoutes/TeacherRoutes"
+import { me } from "./services/tasks2.service"
+import Login from "./pages/login/Login"
+
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<Navigate to="/teachers/tasks" replace />} />
-          <Route path="/hello" element={<Hello />} />
-          <Route path="teachers/*" element={<TeacherRoutes />} />
-          {/* <Route path="/tasks" element={<TaskPage />} />
+  const token = localStorage.getItem("token")
+
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        const user = await me()
+        setUser(user)
+        console.log("user: ", user)
+      } catch (e) {}
+    }
+    fetchMe()
+  }, [])
+  if (token && user.role === "admin") {
+    // private routes
+    return (
+      <div className="app">
+        <Router>
+          <Menu />
+          <Routes>
+            <Route
+              path="/"
+              element={<Navigate to="/teachers/tasks" replace />}
+            />
+            <Route path="/hello" element={<Hello />} />
+            <Route path="teachers/*" element={<TeacherRoutes />} />
+            {/* <Route path="/tasks" element={<TaskPage />} />
           <Route path="/tasks/:id" element={<TaskDetails />} /> */}
+          </Routes>
+        </Router>
+      </div>
+    )
+  } else if (token && user.role === "user") {
+    return (
+      <div className="app">
+        <Router>
+          <Menu />
+          {/* ... */}
+        </Router>
+      </div>
+    )
+  } else if (!token) {
+    // public routes
+    return (
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
-    </div>
-  )
+    )
+  } else {
+    return <div>loading...</div>
+  }
 }
 
 export default App
---
+++ src/pages/login/Login.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import { login } from "../../services/tasks2.service"
+import { useNavigate } from "react-router-dom"
+function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+  const handleClick = async () => {
+    const token = await login({ email, password })
+    console.log("token: ", token)
+    localStorage.setItem("token", token)
+    // navigate("/")
+    window.location = "/"
+  }
+  return (
+    <div>
+      <div>
+        Email
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={handleClick}>Se connecter</button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
