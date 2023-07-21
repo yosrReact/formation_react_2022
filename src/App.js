7d8f7a8bc4dcac2539485885f46a18b3eaf26dae
Yosr Naija <yosr.naija@live.com>
2023-07-21 08:50:43 +0100

    sass

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import "./App.css"
 import Hello from "./components/hello/Hello"
 import TaskPage from "./pages/tasksPage/TaskPage"
+import ExampleSass from "./pages/exampleSass/ExampleSass"
 
 function App() {
   return (
     <div className="app">
-      <Hello />
-      <TaskPage />
+      {/* <Hello />
+      <TaskPage /> */}
+      <ExampleSass />
     </div>
   )
 }
