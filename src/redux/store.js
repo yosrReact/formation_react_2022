a04e886dd40abed6605a2545f1c2bf1bfe53ed21
Yosr Naija <yosr.naija@live.com>
2023-07-31 11:08:21 +0100

    add middleware and devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { rootReducer } from "./reducers"
 import { configureStore } from "@reduxjs/toolkit"
+import { logger } from "redux-logger"
+import { error } from "./middlewares/error"
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(logger, error),
+  devTools: process.env.NODE_ENV !== "production",
 })
 console.log("store", store.getState())
 
