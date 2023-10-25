87470559d5164b317d14c3c3e384e33adaf286a3
Yosr Naija <yosr.naija@live.com>
2023-10-25 15:33:37 +0100

    add hooks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react"
 import "./App.css"
 import Hello from "./components/hello/Hello"
 import TaskPage from "./pages/tasksPage/TaskPage"
+import useScreenSize from "./hooks/useScreenSize"
 
 function App() {
+  const { isMobile } = useScreenSize()
+  console.log("isMobile in app: ", isMobile)
   return (
     <div className="app">
-      <Hello />
+      {isMobile && <Hello />}
       <TaskPage />
     </div>
   )
